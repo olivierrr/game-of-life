7b9229540e438285cd070130e1b237a351c5856b
olivierr <oliviersinbox@gmaill.com>
2014-06-28 10:58:23 -0400

    Added some comments.

--- main.js
+++ main.js
@@ -25,13 +25,11 @@ var startingParticleCount = 500,
     maxVelocity = 2,
     showAge = 1;
 
-//rules
+//rules //TODO: fix
 var rule3 = 5,
     rule2 = 3,
     rule22 = 4,
     rule1 = 0;
-
-var particles = [];
 
 // returns a positive or negative between 0 and @param[max]
 function rand(max){
@@ -41,10 +39,31 @@ function rand(max){
     return num;
 }
 
+//returns a random number between @param[min] and @param[max]
 function getRandomNum(min, max) {
+
     return Math.random() * (max - min) + min;
 }
 
+/*===================================================================
+*   Particle
+*/
+
+//holds Particle instances
+var particles; 
+
+//Particle instance constructor
+function constructParticle(posX, posY, vx, vy) {
+
+    particles.push(new Particle(
+        posX || getRandomNum(0,W),
+        posY || getRandomNum(0,H),
+        vx   || rand(maxVelocity),
+        vy   || rand(maxVelocity)
+    ));
+}
+
+//Particle
 function Particle(posX, posY, vvx, vvy) {
 
     this.self = this;
@@ -56,24 +75,32 @@ function Particle(posX, posY, vvx, vvy) {
     this.age = 0;
 }
 
+//destroy Particle instance references
 Particle.prototype.kill = function(){
+
 	index = particles.indexOf(this);
     particles.splice(index, 1);
 }
 
+//update Particle instance
 Particle.prototype.update = function(){
+
     //update age
     this.age +=1;
+
     //update position
 	this.x += this.vx;
 	this.y += this.vy;
+
     //bounce off edges
 	if(this.x > W) this.vx = -Math.abs(this.vx);
 	if(this.x < 5) this.vx = Math.abs(this.vx);
 	if(this.y > H) this.vy = -Math.abs(this.vy);
 	if(this.y < 5) this.vy = Math.abs(this.vy);
+
     //reset neighbor count
     this.neighbors = 0;
+
     //find neighbors
     var i = particles.length;
     while(i--){
@@ -83,14 +110,20 @@ Particle.prototype.update = function(){
     }
 }
 
+//draw Particle instance
 Particle.prototype.draw = function(){
+
     var color = '#ADFF2F';
     var size = 2;
+
+    //TODO: fix wall o' ifs
     if(this.age>10) {color = '#008000';}
     if(this.age>30) {color = '#00FA9A';}
     if(this.age>50) {color = '#4682B4'; size = 3}
     if(this.age>80) {color = '#4169E1'; size = 3}
     if(this.age>150) {color = '#8A2BE2'; size = 4}
+
+    //draw Particle
     ctx.beginPath;
     ctx.arc(this.x, this.y, size, 0, 2 * Math.PI, false);
     ctx.fillStyle = color;
@@ -99,10 +132,15 @@ Particle.prototype.draw = function(){
 }
 
 function update() {
+
     //check rules
     rules();
+
+    //clear canvas
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0,W,H);
+
+    //itterate Particle instances
     var i = particles.length;
     while(i--){
         particles[i].update();
@@ -110,24 +148,34 @@ function update() {
     }
 }
 
+//checks game rules
 function rules(){
+
+    //iterrate Particle instances
     var i = particles.length;
     while(i--){
         var p1 = particles[i];
         if( p1.neighbors === rule1 ) p1.kill();
         if( p1.neighbors > rule3 ) p1.kill();
-        if( p1.neighbors === rule2 || p1.neighbors === rule22) createParticle(
+        if( p1.neighbors === rule2 || p1.neighbors === rule22) constructParticle(
             p1.x + rand(minDist/expandDivider), p1.y + rand(minDist/expandDivider), p1.vx, p1.vy
         );
     }
 }
 
+//calculates and draws vector
 function getVector(p1, p2) {
+
+    //get distance between p1 and p2
     var dx = p1.x - p2.x,
-        dy = p1.y - p2.y;
-    var dist = Math.sqrt(dx*dx + dy*dy);
+        dy = p1.y - p2.y,
+        dist = Math.sqrt(dx*dx + dy*dy);
+
     if(dist <= minDist) {
+
+        //update neighbor count
         p1.neighbors += 1;
+
         //draw vector
         ctx.beginPath();
         ctx.strokeStyle = "rgba(255,255,255,"+ (1.2-dist/minDist) +")";
@@ -138,37 +186,41 @@ function getVector(p1, p2) {
     }
 }
 
-function createParticle(posX, posY, vx, vy) {
-    particles.push(new Particle(
-        posX || getRandomNum(0,W),
-        posY || getRandomNum(0,H),
-        vx   || rand(maxVelocity),
-        vy   || rand(maxVelocity)
-    ));
-}
-
+//game loop
 function loop() {
+
     if(gameState === 1){
         linesCounter = 0;
         update();
     }
+
     requestAnimFrame(loop);
 }
 
+//initiate new simulation
 function init(){
+
+    //clear Particle instance array
     particles = [];
+
+    //spawn initial Particles
 	var i = startingParticleCount;
 	while(i--){
-		createParticle();
+		constructParticle();
 	}
 }
 
 init();
 loop();
 
-//dom
+/*===========================================================================
+*   DOM
+*/
+
+//TODO: fix this... all of this V
+
 canvas.onmousedown = function(e) {
-    canvas.onmousemove = function(e) { createParticle(e.pageX, e.pageY); }
+    canvas.onmousemove = function(e) { constructParticle(e.pageX, e.pageY); }
     document.onmouseup = function() { canvas.onmousemove = null; }
 }
 
